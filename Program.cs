e13a497a054fa245e3f1a3804356a05dac3b45a1
Iryna <kytsenkoiryna33@gmail.com>
2025-11-02 23:59:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Cart
+{
+    public string State { get; set; }
+    public string Save() => State;
+    public void Restore(string memento) => State = memento;
+}
+
+class Program
+{
+    static void Main()
+    {
+        var cart = new Cart();
+        cart.State = " 1 троянда";
+        string backup = cart.Save();
+
+        cart.State = " 1 троянда, 1 тюльпан";
+        cart.Restore(backup);
+
+        Console.WriteLine("Відновлено стан: " + cart.State);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
